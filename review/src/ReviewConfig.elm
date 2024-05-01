5d7fc30a75e97a13089eae39d147a262c5b9e9bd
VladimirLogachev <vladimir@logachev.dev>
2024-05-01 18:54:18 +0400

    Add elm-review

---
+++ review/src/ReviewConfig.elm
@@ -0,0 +1,66 @@
+module ReviewConfig exposing (config)
+
+{-| Do not rename the ReviewConfig module or the config function, because
+`elm-review` will look for these.
+
+To add packages that contain rules, add them to this review project using
+
+    `elm install author/packagename`
+
+when inside the directory containing this file.
+
+-}
+
+import NoDebug.Log
+import NoDebug.TodoOrToString
+import NoExposingEverything
+import NoImportingEverything
+import NoMissingSubscriptionsCall
+import NoMissingTypeAnnotation
+import NoMissingTypeAnnotationInLetIn
+import NoPrematureLetComputation
+import NoRecursiveUpdate
+import NoSimpleLetBody
+import NoTestValuesInProductionCode
+import NoUnoptimizedRecursion
+import NoUnsafeDivision
+import NoUnused.CustomTypeConstructorArgs
+import NoUnused.CustomTypeConstructors
+import NoUnused.Dependencies
+import NoUnused.Exports
+import NoUnused.Modules
+import NoUnused.Parameters
+import NoUnused.Patterns
+import NoUnused.Variables
+import NoUselessSubscriptions
+import Review.Rule exposing (Rule)
+import Simplify
+
+
+config : List Rule
+config =
+    [ NoDebug.Log.rule
+    , NoDebug.TodoOrToString.rule
+    , NoUnoptimizedRecursion.rule (NoUnoptimizedRecursion.optOutWithComment "IGNORE TCO")
+    , Simplify.rule Simplify.defaults
+    , NoTestValuesInProductionCode.rule
+        (NoTestValuesInProductionCode.startsWith "stub_")
+    , NoMissingSubscriptionsCall.rule
+    , NoRecursiveUpdate.rule
+    , NoUselessSubscriptions.rule
+    , NoUnsafeDivision.rule
+    , NoUnused.CustomTypeConstructors.rule []
+    , NoUnused.CustomTypeConstructorArgs.rule
+    , NoUnused.Dependencies.rule
+    , NoUnused.Exports.rule
+    , NoUnused.Modules.rule
+    , NoUnused.Parameters.rule
+    , NoUnused.Patterns.rule
+    , NoExposingEverything.rule
+    , NoUnused.Variables.rule
+    , NoImportingEverything.rule [ "Element", "Html" ]
+    , NoMissingTypeAnnotation.rule
+    , NoMissingTypeAnnotationInLetIn.rule
+    , NoPrematureLetComputation.rule
+    , NoSimpleLetBody.rule
+    ]

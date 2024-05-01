624e0cfb35a69e58bfd7acbba3ad33ae73071441
VladimirLogachev <vladimir@logachev.dev>
2024-05-02 01:27:13 +0400

    Try

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -31,7 +31,7 @@ export default defineConfig(({ command }) => {
     plugins: [elmPlugin(elmSettings)],
     server: { host, port, strictPort: true },
     build: {
-      base: "/elm-modular-grid/",
+      // base: "/elm-modular-grid/",
       rollupOptions: {
         input: {
           // @ts-ignore
